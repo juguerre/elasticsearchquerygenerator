c32009fa7ffaed88ff357a7545b9cffe0e1e47a9
juguerre <juguerre@gmail.com>
2022-08-27 18:37:35 +0200

    setup.py refactor

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
+import os
+import site
+import sys
+
 from setuptools import setup
 
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
-def readme():
-    with open("README.md") as f:
-        README = f.read()
-    return README
+with open("./requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
+print(os.getcwd())
+
+with open("./README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 
 setup(
@@ -14,18 +22,19 @@ setup(
     Create Complex Elastic Search Query in Seconds
     Please see documentation for more details
      """,
-    long_description=readme(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/soumilshah1995/elasticsearchquerygenerator.git",
-    author="Soumil Nitin Shah",
-    author_email="soushah@my.bridgeport.edu",
+    url="https://github.com/juguerre/elasticsearchquerygenerator.git",
+    author="Andres Guerrero",
+    author_email="juguerre@gmail.com",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     packages=["elasticsearchquerygenerator"],
     include_package_data=True,
-    install_requires=[],
+    install_requires=install_requires,
+    python_requires="<3.9",
 )
